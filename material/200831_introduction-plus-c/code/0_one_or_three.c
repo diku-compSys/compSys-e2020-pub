42e54e7b3579cb4570458142c138b98ba05c6573
Michael Kirkedal Thomsen <m.kirkedal@di.ku.dk>
2020-09-02 03:23:11 +0200

    Currection of date

--- material/200831_introduction-plus-c/code/0_one_or_three.c
+++ material/200831_introduction-plus-c/code/0_one_or_three.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char* argv[]) {
   if (argc == 2) { // 1 argument
+  	printf("One arguments: %s\n", argv[0]);
     return EXIT_SUCCESS;
   }
   else if (argc == 4) { // 3 arguments
+  	printf("Three arguments: %s\n", argv[0]);
     return EXIT_SUCCESS;
   }
   else {
