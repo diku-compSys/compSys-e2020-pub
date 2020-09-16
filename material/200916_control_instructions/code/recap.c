4b51f30fc6ef6c770962ee35ea80b7f3d0db24cd
Michael Kirkedal Thomsen <m.kirkedal@di.ku.dk>
2020-09-16 09:19:06 +0200

    Update to recap example

--- material/200916_control_instructions/code/recap.c
+++ material/200916_control_instructions/code/recap.c
@@ -1,7 +1,11 @@
 
-long sumOfTwoFirst (long *x) {
+
+// x is an array of long
+// i is an index into the array
+
+long weightedSumOfTwo (long *x, unsigned i) {
   long rval = (*x) * 7;
-  x++;
+  x += i;
   rval += (*x)*5;
   return rval;
 }
--- material/200916_control_instructions/code/recap.s
+++ material/200916_control_instructions/code/recap.s
@@ -1,19 +1,20 @@
-	.file	"caaee660226dddb4bbcf52b2065f65b0.c"
+	.file	"0b93edeca53a44db475acf9640f99c10.c"
 	.text
-	.globl	sumOfTwoFirst
-	.type	sumOfTwoFirst, @function
-sumOfTwoFirst:
+	.globl	weightedSumOfTwo
+	.type	weightedSumOfTwo, @function
+weightedSumOfTwo:
 .LFB0:
 	.cfi_startproc
 	movq	(%rdi), %rdx
 	leaq	0(,%rdx,8), %rax
 	subq	%rdx, %rax
-	movq	8(%rdi), %rdx
+	movl	%esi, %esi
+	movq	(%rdi,%rsi,8), %rdx
 	leaq	(%rdx,%rdx,4), %rdx
 	addq	%rdx, %rax
 	ret
 	.cfi_endproc
 .LFE0:
-	.size	sumOfTwoFirst, .-sumOfTwoFirst
+	.size	weightedSumOfTwo, .-weightedSumOfTwo
 	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.12) 5.4.0 20160609"
 	.section	.note.GNU-stack,"",@progbits
