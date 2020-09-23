1e8c2a694c44bb6cc7af062067adfd875d1ef2c6
Michael Kirkedal Thomsen <m.kirkedal@di.ku.dk>
2020-09-23 16:10:28 +0200

    Simple install script

---
+++ x86prime_tools/install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+   echo "[CompSys] Scripts for setting up x86prime tools."
+   echo "Usage:"
+   echo "  ./install.sh [dir]"
+   echo "    dir:  The absolut tid to your tool location. On MacOS starts likely with /Users/"
+   exit 1;
+fi
+
+declare -a tools=("gcc-online" "gdb-online" "prasm" "prerf" "primify" "prun")
+x86prime_path=$1
+
+if [[ ! -d "${x86prime_path}" ]] ; then
+  echo "The given directory does not exist!"
+  exit
+fi
+
+for tool in ${tools[@]}; do
+  if [[ ! -e "${x86prime_path}/${tool}.py" ]] ; then
+    echo "Cannot find ${tool}.py in ${x86prime_path} !!!"
+    exit
+  fi
+done
+
+echo ${x86prime_path} > ~/.x86prime_path
+
+for tool in ${tools[@]}; do
+  echo '#!/usr/bin/env bash
+
+set -euo pipefail
+args=$(echo "${*:1}")
+x86p_path=$(cat ~/.x86prime_path)
+python3 ${x86p_path}/'${tool}'.py $args
+' > /usr/local/bin/${tool}
+  chmod a+x /usr/local/bin/${tool}
+done
