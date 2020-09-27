5e6f47fbeaf965addbcc189473cc3f8f2573ca31
Michael Kirkedal Thomsen <m.kirkedal@di.ku.dk>
2020-09-27 16:07:00 +0200

    Comments in install script

--- x86prime_tools/install.sh
+++ x86prime_tools/install.sh
@@ -2,14 +2,18 @@
 
 echo ">> Installing x86prime tools."
 
+# The list of tools
 declare -a tools=("gcc-online" "gdb-online" "prasm" "prerf" "primify" "prun")
+# Fint my path
 x86prime_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Does the path exist?
 if [[ ! -d "${x86prime_path}" ]] ; then
   echo "The given directory does not exist!"
   exit
 fi
 
+# Does the tools exist?
 for tool in ${tools[@]}; do
   if [[ ! -e "${x86prime_path}/${tool}.py" ]] ; then
     echo "Cannot find ${tool}.py in ${x86prime_path} !!!"
@@ -17,8 +21,10 @@ for tool in ${tools[@]}; do
   fi
 done
 
+# Path for tools
 mkdir -p /usr/local/lib/x86prime
 
+# Move each tool and create executable script for these.
 for tool in ${tools[@]}; do
   cp ${x86prime_path}/${tool}.py /usr/local/lib/x86prime/
   echo '#!/usr/bin/env bash
