315587accb605a1f95bfbe2f96b2139161ff573e
Troels Henriksen <athas@sigkill.dk>
2020-10-27 17:35:58 +0100

    Avoid overflow.

--- material/201028_virtual_memory_i/mem0.c
+++ material/201028_virtual_memory_i/mem0.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-  int num_gib = atoi(argv[1]);
+  size_t num_gib = atoi(argv[1]);
   size_t to_alloc = num_gib * 1024 * 1024 * 1024;
+
+  printf("allocating %ld bytes\n", (long)to_alloc);
 
   unsigned char *p = malloc(to_alloc);
 
