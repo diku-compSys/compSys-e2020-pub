dd649f0cef5b9ca6031628dd09df7892f8cfd17f
Troels Henriksen <athas@sigkill.dk>
2020-11-02 12:15:20 +0100

    Final polish.

--- material/201102_virtual_memory_ii/mallocs.c
+++ material/201102_virtual_memory_ii/mallocs.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main() {
   for (int i = 1; i < 30; i++) {
     int num_bytes = 1 << i;
-    printf("malloc(%d)\n", num_bytes);
-    malloc(num_bytes);
+    printf("malloc(%d): ", num_bytes);
+    printf("%p\n", malloc(num_bytes));
   }
 }
--- material/201102_virtual_memory_ii/small_mallocs.c
+++ material/201102_virtual_memory_ii/small_mallocs.c
@@ -4,7 +4,6 @@
 int main() {
   for (int i = 1; i < 30000000; i++) {
     int num_bytes = 64;
-    printf("malloc(%d)\n", num_bytes);
-    malloc(num_bytes);
+    printf("malloc(%d) = %p\n", num_bytes, malloc(num_bytes));
   }
 }
